187ee3eaa6d08be960b4dee5d1afbe41cb2c2327
SlpzZ <maxzindy@gmail.com>
2020-06-03 16:57:23 +0700

    Create Exercise4_Vichayut_E.py

---
+++ Exercise4_Vichayut_E.py
@@ -0,0 +1,8 @@
+fe = 65.5
+gb = 75.5
+ic = 85.5
+cp = 95.5
+print("Foundation English :", fe)
+print("General Business :", gb)
+print("Introduction to Computer Systems :", ic)
+print("Computer Programming :", cp)
